bd498e0cddd7818c45cb181303d55651a305fd73
Daniel Sosinskiy <danyava13@gmail.com>
2020-03-02 18:01:52 +0200

    S7 E102 wrapping multiple elements V.3 (Aux)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,7 +7,7 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <React.Fragment>
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
@@ -15,7 +15,7 @@ class Person extends Component {
                     onChange={this.props.changed} 
                     value={this.props.name}
                 />
-            </React.Fragment>
+            </Aux>
         )
     }
 };
